ffb17e93b5a1b8ea870e7d77250f8094a8d43239
Anna Tiala <anna.tiala@vaimo.com>
2024-02-26 14:42:56 +0200

    add backgroundimage for header

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,37 +1,46 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
 import Button from '../components/Button'
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.introText}>
-                    Tervetuloa Arkiapuriin! Täältä pääset siirtymään aterioiden
-                    suunnitteluun ja selaamiseen, lukujärjestykseesi,
-                    ruokakomeroon sekä ostoslistaan.
-                </Text>
+        <View style={styles.homeView}>
+            <View style={styles.header}>
+                <ImageBackground
+                    style={styles.backgroundImage}
+                    source={require('../assets/images/letut.png')}
+                >
+                    <View style={styles.layer}>
+                        <Text style={styles.introText}>
+                            Tervetuloa Arkiapuriin! Täältä pääset siirtymään
+                            aterioiden suunnitteluun ja selaamiseen,
+                            lukujärjestykseesi, ruokakomeroon sekä ostoslistaan.
+                        </Text>
+                    </View>
+                </ImageBackground>
             </View>
-            <Button
-                style={styles.pantryButton}
-                title="Ruokakomero"
-                onPress={() => navigation.navigate('Pantry')}
-            />
-            <Button
-                style={styles.mealsButton}
-                title="Ateriat"
-                onPress={() => navigation.navigate('Meals')}
-            />
-            <Button
-                style={styles.readingOrderButton}
-                title="Lukujärjestys"
-                onPress={() => navigation.navigate('Reading order')}
-            />
-            <Button
-                style={styles.shoppingListButton}
-                title="Ostoslista"
-                onPress={() => navigation.navigate('Shopping list')}
-            />
+            <View style={styles.container}>
+                <Button
+                    style={styles.pantryButton}
+                    title="Ruokakomero"
+                    onPress={() => navigation.navigate('Pantry')}
+                />
+                <Button
+                    style={styles.mealsButton}
+                    title="Ateriat"
+                    onPress={() => navigation.navigate('Meals')}
+                />
+                <Button
+                    style={styles.readingOrderButton}
+                    title="Lukujärjestys"
+                    onPress={() => navigation.navigate('Reading order')}
+                />
+                <Button
+                    style={styles.shoppingListButton}
+                    title="Ostoslista"
+                    onPress={() => navigation.navigate('Shopping list')}
+                />
+            </View>
         </View>
     )
 }
@@ -39,9 +48,15 @@ const HomeScreen = ({ navigation }) => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
+    homeView: {
+        backgroundColor: '#fff',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
@@ -52,10 +67,17 @@ const styles = StyleSheet.create({
         padding: 20,
         marginBottom: 10,
     },
-    mealsButton: {
-        // marginBottom: 20,
+    header: {
+        minHeight: 400,
     },
-    radingOrderButton: {
-        // marginBottom: 20,
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        position: 'relative',
+    },
+    layer: {
+        backgroundColor: 'rgba(248, 247, 216, 0.7)',
+        width: '100%',
+        height: '100%',
     },
 })
