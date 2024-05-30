6a6d289d862c11a783d08385dffca70638a30e3b
Anna Tiala <anruntti@gmail.com>
2024-05-30 15:10:41 +0300

    add package and style fixes

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,7 +1,11 @@
 import * as React from 'react'
 import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 import Button from '../components/Button'
-import images from '../assets/images'
+
+const image = {
+    uri: 'https://images.ctfassets.net/hef5a6s5axrs/3OzNmAlq5mzLJyKIJSlGaX/a02fc998f812267ea2c349285897dd5c/IMG_1682.jpg',
+}
 
 const StartScreen = ({ navigation }) => {
     const onSignUpPress = () => {
@@ -9,16 +13,21 @@ const StartScreen = ({ navigation }) => {
     }
     return (
         <View style={styles.loginView}>
-            <View style={styles.header}>
+            <LinearGradient
+                // Background Linear Gradient
+                colors={['rgba(0,0,0,0.8)', 'transparent']}
+                style={styles.background}
+            >
                 <ImageBackground
-                    style={{
-                        width: '100%', // applied to Image
-                        height: '100%',
-                    }}
-                    imageStyle={{
-                        resizeMode: 'covern', // works only here!
-                    }}
-                    source={images.startViewImage}
+                    // style={{
+                    //     width: '100%', // applied to Image
+                    //     height: '100%',
+                    // }}
+                    // imageStyle={{
+                    //     resizeMode: 'cover', // works only here!
+                    // }}
+                    style={styles.image}
+                    source={image}
                 >
                     <View style={styles.layer}>
                         <View style={styles.textContentArea}>
@@ -34,7 +43,7 @@ const StartScreen = ({ navigation }) => {
                         </View>
                     </View>
                 </ImageBackground>
-            </View>
+            </LinearGradient>
         </View>
     )
 }
@@ -43,11 +52,17 @@ export default StartScreen
 
 const styles = StyleSheet.create({
     loginView: {
-        backgroundColor: '#fff',
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         width: '100%',
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: '90vh',
     },
     textContentArea: {
         flex: 1,
@@ -55,6 +70,7 @@ const styles = StyleSheet.create({
         justifyContent: 'top',
         width: '100%',
         padding: 40,
+        backgroundColor: '#000000a0',
     },
     introText: {
         fontSize: 25,
@@ -83,5 +99,10 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         width: 'auto',
+    },
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
     },
 })
