738cc54833e9cf5075c9a40607162cc425ae82b5
annaruntti <anruntti@gmail.com>
2025-02-05 13:34:02 +0200

    fix button's android styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,24 +3,38 @@ import { Pressable, StyleSheet } from 'react-native'
 import CustomText from './CustomText'
 
 export default function Button(props) {
-    const { onPress, title = 'Tallenna', style } = props
+    const { onPress, title = 'Tallenna', style, textStyle } = props
     return (
         <Pressable
             style={({ pressed }) => [
                 styles.button,
-                style,
                 pressed && styles.pressed,
+                style,
             ]}
             onPress={onPress}
             android_ripple={{ color: 'transparent' }}
         >
-            <CustomText style={style}>{title}</CustomText>
+            <CustomText style={[styles.text, textStyle]}>{title}</CustomText>
         </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#9C86FC', //default color
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
     pressed: {
         opacity: 0.75,
+    },
+    text: {
+        color: '#000', // default color
+        fontWeight: 'bold',
+        padding: 7,
     },
 })
