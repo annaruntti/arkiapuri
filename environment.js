3f3f6078c830901a158392c0373da9e0cf19baa5
annaruntti <anruntti@gmail.com>
2025-10-31 16:10:08 +0200

    change info for ip address usage for testing

--- environment.js
+++ environment.js
@@ -1,9 +1,9 @@
 const ENV = {
     development: {
         // For mobile device testing with Expo Go, use your computer's IP
-        // apiUrl: 'http://192.168.50.179:3000',
+        apiUrl: 'http://<your-ip-address>:3000',
         // For web testing, use:
-        apiUrl: 'http://localhost:3000',
+        // apiUrl: 'http://localhost:3000',
     },
     production: {
         apiUrl: 'https://arkiapuri-api-production.up.railway.app', // Railway backend domain
